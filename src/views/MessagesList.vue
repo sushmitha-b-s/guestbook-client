b927fcd9ed0d8b09ea227df3fbcdafa588fa1ccc
Sushmitha BS <bssushmitha26@gmail.com>
2020-06-30 15:53:07 +0200

    rendered all the messages

--- src/views/MessagesList.vue
+++ src/views/MessagesList.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="messages-list">
-    <h2>There are no messages yet</h2>
+    <MessagesItem
+      v-for="message in messages"
+      :key="message.id"
+      :message="message"
+    />
+
+    <button>+ Create message</button>
   </div>
 </template>
+
+<script>
+import MessagesItem from '@/components/MessagesItem.vue'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'MessagesList',
+  components: {
+    MessagesItem
+  },
+
+  created() {
+    this.$store.dispatch('guestbook/fetchMessages')
+  },
+
+  computed: {
+    ...mapGetters({
+      messages: 'guestbook/messages'
+    })
+  }
+}
+</script>
